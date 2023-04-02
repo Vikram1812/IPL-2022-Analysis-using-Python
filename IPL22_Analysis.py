846158d2be8b361f55018c207fcde5baffdd718c
Vikram <80635017+Vikram1812@users.noreply.github.com>
2023-04-02 14:59:53 +0530

    Add files via upload

---
+++ IPL22_Analysis.py
@@ -0,0 +1,104 @@
+#Loading packages
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+
+#Loading dataset
+data = pd.read_csv("IPL 2022.csv")
+print(data.head())
+
+#Number of matches won by each team
+figure = px.bar(data, x=data["match_winner"],
+            title="Number of Matches Won in IPL 2022")
+figure.show()
+
+#Number of matches won by defending or chasing
+data["won_by"] = data["won_by"].map({"Wickets": "Chasing", 
+                                     "Runs": "Defending"})
+won_by = data["won_by"].value_counts()
+label = won_by.index
+counts = won_by.values
+colors = ['gold','lightgreen']
+
+fig = go.Figure(data=[go.Pie(labels=label, values=counts)])
+fig.update_layout(title_text='Number of Matches Won By Defending Or Chasing')
+fig.update_traces(hoverinfo='label+percent', textinfo='value', 
+                  textfont_size=30,
+                  marker=dict(colors=colors, 
+                              line=dict(color='black', width=3)))
+fig.show()
+
+#Toss Decision(Batting or Fielding)
+toss = data["toss_decision"].value_counts()
+label = toss.index
+counts = toss.values
+colors = ['skyblue','yellow']
+
+fig = go.Figure(data=[go.Pie(labels=label, values=counts)])
+fig.update_layout(title_text='Toss Decision')
+fig.update_traces(hoverinfo='label+percent', 
+                  textinfo='value', textfont_size=30,
+                  marker=dict(colors=colors, 
+                              line=dict(color='black', width=3)))
+fig.show()
+
+#Top Scorers in IPL22
+figure = px.bar(data, x=data["top_scorer"],
+            title="Top Scorers in IPL 2022")
+figure.show()
+
+#Top Scorers(Runs)
+figure = px.bar(data, x=data["top_scorer"], 
+                y = data["highscore"], 
+                color = data["highscore"],
+            title="Top Scorers in IPL 2022")
+figure.show()
+
+#Most PotM
+figure = px.bar(data, x = data["player_of_the_match"], 
+                title="Most Player of the Match Awards")
+figure.show()
+
+#Best Bowlers in IPL22
+figure = px.bar(data, x=data["best_bowling"],
+            title="Best Bowlers in IPL 2022")
+figure.show()
+
+#Number of wickets in each innings
+figure = go.Figure()
+figure.add_trace(go.Bar(
+    x=data["venue"],
+    y=data["first_ings_wkts"],
+    name='First Innings Wickets',
+    marker_color='gold'
+))
+figure.add_trace(go.Bar(
+    x=data["venue"],
+    y=data["second_ings_wkts"],
+    name='Second Innings Wickets',
+    marker_color='lightgreen'
+))
+figure.update_layout(barmode='group', xaxis_tickangle=-45)
+figure.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
